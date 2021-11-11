e49a8ac4e596a9fd2ba31e5cb67ea4e06366ee93
David Leach <davidleach724@gmail.com>
2021-11-11 15:29:15 -0700

    Add url to click

--- src/components/CardDetails/CardDetails.js
+++ src/components/CardDetails/CardDetails.js
@@ -1,12 +1,15 @@
 import './CardDetails.css'
+const dayjs = require('dayjs');
+
 
 const CardDetails = ({ story }) => {
-  console.log(story)
 
   return (
     <div className="card-details">
       <h2> {story.title}</h2>
+      <h4> { story.byline } </h4>
       <img className="larger-picture" src={ story.multimedia[0].url} />
+      <h4> {dayjs(story.published_date).format('MMMM DD, YYYY')}</h4>
     </div>
   )
 }
--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,20 +1,16 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import CardDetails from '../CardDetails/CardDetails'
 import './NewsCard.css'
 
 const NewsCard = ({ story }) => {
-  const [selectedArticle, setSelectedArticle] = useState(null)
   const [hoverStatus, setHoverStatus] = useState(false)
-
-  // useEffect(() => {
-  //   console.log(story)
-  // }, [hoverstatus])
 
   return (
     <>
     <div className="news-card"  
       onMouseEnter={() => setHoverStatus(true)}
       onMouseLeave={() => setHoverStatus(false)}
+      onClick={() => window.open(story.url)}
       >
       <img className='thumbnail' src={ story.multimedia[0].url } />
     </div>

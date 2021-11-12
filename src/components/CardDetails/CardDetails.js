b7082577ec765222746822f0a3d82aef20be3e06
David Leach <davidleach724@gmail.com>
2021-11-12 09:43:00 -0700

    Style card details for mobile

--- src/components/CardDetails/CardDetails.js
+++ src/components/CardDetails/CardDetails.js
@@ -6,8 +6,8 @@ const CardDetails = ({ story }) => {
 
   return (
     <div className="card-details">
-      <h2> {story.title}</h2>
-      <h4> { story.byline } </h4>
+      <h2 className='story-title'> {story.title}</h2>
+      <h4 className='story-author'> { story.byline } </h4>
       <img className="larger-picture" src={ story.multimedia[0].url} />
       <h4> {dayjs(story.published_date).format('MMMM DD, YYYY')}</h4>
     </div>
